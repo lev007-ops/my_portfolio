c8072347bad4714523e7b5cfa73c9fcdc4541b63
Lev <lev_bariakh@icloud.com>
2022-10-09 20:22:30 +0500

    test

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
