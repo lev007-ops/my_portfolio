523758c6d3f47664a09fcb02e5fb0c42d63a7e02
Lev <lev_bariakh@icloud.com>
2022-10-29 11:37:35 +0500

    add github to about page

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
