4801a7496f9c26d7be8a96e232c77761c3d6745f
Lev <lev_bariakh@icloud.com>
2022-10-29 22:30:09 +0500

    fix about page

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,12 +18,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from .settings import STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
     path("blog/", include("blog.urls")),
-    path("about-me/", include("about_me.urls")),
+    path("about-me/", include("about_me.urls"))
 ]
 
 if settings.DEBUG:
