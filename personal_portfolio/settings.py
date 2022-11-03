61bfb210ae81933113ab8e95e30955ceffff4756
Lev <lev_bariakh@icloud.com>
2022-11-03 21:47:34 +0500

    fix favicon

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from environs import Env
+import os
 
 env = Env()
 env.read_env()
@@ -129,8 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
