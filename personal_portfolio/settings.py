253571093bfe369210d598300a2df9a5ee79ed07
Lev <lev_bariakh@icloud.com>
2022-11-04 12:16:31 +0500

    update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -130,9 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "staticfiles/favicons"),
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
